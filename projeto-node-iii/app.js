539caf1db93ac35d25b0a35710dd8de4edf2a7bb
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2023-04-26 11:11:19 -0300

    update

--- projeto-node-iii/app.js
+++ projeto-node-iii/app.js
@@ -3,19 +3,30 @@ const app = express()
 
 const handlebars = require('express-handlebars').engine
 
+
 const bodyParser = require('body-parser')
 const post = require('./models/post')
+
+
 
 //Configurando o template engine 
 app.engine("handlebars", handlebars({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
+
+handlebars.registerHelper('equals', function(origem, valor){
+    if(origem === valor){
+        return true 
+    }
+    return false
+})
+
 
 //Configurando o body-parser
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get("/", function(req, res){
-    res.render("first_page")
+    res.render("index")
 })
 
 //Os dados do form serão enviados via POST
@@ -36,9 +47,28 @@ app.post("/cadastrar", function(req, res){
 //Pegando dados pra apresentar
 app.get("/consulta", function(req, res){
     post.findAll().then(function(post){
-        res.render("second_page", {post})
+        res.render("consultar", {post})
     }).catch(function(erro){
         console.log("Erro ao carregar dados"+erro)
+    })
+})
+
+//Rota para alterar
+app.get("/alterar/:id", function(req, res){
+    // post.findAll().then(function(post){
+    //     res.render("consultar", {post})
+    // }).catch(function(erro){
+    //     console.log("Erro ao carregar dados"+erro)
+    // })
+    post.findAll({
+        where:{
+            'id': req.params.id
+        }
+    }).then(function(post){
+        res.render('atualizar', {post})
+        console.log(post)
+    }).catch(function(erro){
+        console.log("erro: "+erro)
     })
 })
 
@@ -51,14 +81,14 @@ app.get("/excluir/:id", function(req, res){
     }).then(() => {
         res.redirect("/consulta")
     }).catch((erro) => {
-        console.log("Erro ao carregar dados"+erro);
+        console.log("Erro ao carregar dados"+erro)
     })
   
 })
 
 //Rota para a segunda página
 app.get("/sec", function(req, res){
-    res.render("second_page")
+    res.render("consultar")
 })
 
 app.listen(8081, function(){
